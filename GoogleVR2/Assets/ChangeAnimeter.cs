9c775fdedbdd4adebc8ea76d6acd9eae3e2599f9
FtotheU <anon.saeheng6009@gmail.com>
2022-02-28 20:47:58 +0700

    first commit

---
+++ GoogleVR2/Assets/ChangeAnimeter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimetor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void getAnimetorHipHop(){
+        this.GetComponent<Animetor>().Play("Hip Hop Dancing");
+    }
+    public void getAnimetorSalsa(){
+        this.GetComponent<Animetor>().Play("Salsa Dancing");
+    }
+}
